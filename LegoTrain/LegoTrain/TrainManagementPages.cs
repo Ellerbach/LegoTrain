103b5b75b077df4fc1572a63c3735f08df76c20a
Laurent Ellerbach <laurent@ellerbach.net>
2016-07-20 13:22:17 +0200

    Adding light support in circuitand default page, adding Excel file to help creating the setup file

--- LegoTrain/LegoTrain/TrainManagementPages.cs
+++ LegoTrain/LegoTrain/TrainManagementPages.cs
@@ -728,30 +728,44 @@ namespace LegoTrain
             strResp += "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head><title>Gestion des trains</title>";
             //this is the css to make it nice :-)
             strResp += "<link href=\"file/" + pageCSS + "\" rel=\"stylesheet\" type=\"text/css\" />";
-            strResp += "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body onLoad=\"getswitches();\">";
+            strResp += "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body onLoad=\"InitAll();\">";
             strResp += "<meta http-equiv=\"Cache-control\" content=\"no-cache\"/>";
             //create the script part
             strResp += "<SCRIPT language=\"JavaScript\">";
-            strResp += "var xhr = new XMLHttpRequest(); function btnclicked(boxMSG, cmdSend) { boxMSG.innerHTML=\"Waiting\";";
+            strResp += "function btnclicked(boxMSG, cmdSend) { var xhr = new XMLHttpRequest(); boxMSG.innerHTML=\"Waiting\";";
             strResp += "xhr.open('GET', cmdSend + '&" + securityKey + "&_=' + Math.random());";
             strResp += "xhr.send(null); xhr.onreadystatechange = function() {if (xhr.readyState == 4) {boxMSG.innerHTML=xhr.responseText;}};}";
 
             strResp += "function findTop(iobj) { ttop = 0; while (iobj) { ttop += iobj.offsetTop; iobj = iobj.offsetParent; } return ttop; }";
             strResp += "function findLeft(iobj) { tleft = 0; while (iobj) { tleft += iobj.offsetLeft; iobj = iobj.offsetParent; } return tleft; }";
             //request switch change
-            strResp += "var xhr = new XMLHttpRequest(); function swclicked(boxMSG, cmdSend) { var mycmd = cmdSend +'&md='; ";
+            strResp += "function swclicked(boxMSG, cmdSend) { var xhr = new XMLHttpRequest(); var mycmd = cmdSend +'&md='; ";
             strResp += "if (boxMSG.src.indexOf('turn.png')>=0) mycmd+='0'; else mycmd += '1';";
             strResp += "xhr.open('GET', 'switch.aspx?' + mycmd + '&" + securityKey + "&_='+Math.random());";
             strResp += "xhr.send(null); xhr.onreadystatechange = function() {if (xhr.readyState == 4) { if(xhr.responseText.indexOf('OK')>=0)";
             strResp += "if (boxMSG.src.indexOf('turn')>=0) boxMSG.src = 'file/str.png'; else boxMSG.src = 'file/turn.png';}};}";
-            //strResp = WebServer.OutPutStream(response, strResp);
+            //request signal change
+            strResp += "function siclicked(boxMSG, cmdSend) { var xhr = new XMLHttpRequest(); var mycmd = cmdSend +'&md='; ";
+            strResp += "if (boxMSG.src.indexOf('green.png')>=0) mycmd+='0'; else mycmd += '1';";
+            strResp += "xhr.open('GET', 'signal.aspx?' + mycmd + '&" + securityKey + "&_='+Math.random());";
+            strResp += "xhr.send(null); xhr.onreadystatechange = function() {if (xhr.readyState == 4) { if(xhr.responseText.indexOf('OK')>=0)";
+            strResp += "if (boxMSG.src.indexOf('red')>=0) boxMSG.src = 'file/green.png'; else boxMSG.src = 'file/red.png';}};}";
             //create the initial value for the switches
             strResp += "function buildSwitch(boxMSG, num) {var obj = document.getElementById(boxMSG); ";
             strResp += "if (num == 1) obj.src = 'file/str.png'; else obj.src = 'file/turn.png';}";
             strResp += "var NumberSwitch=" + mySwitch.NumberOfSwitch + ";";
-            strResp += "function getswitches() {xhr.open('GET', 'switch.aspx?" + securityKey + "&no=1&_=' + Math.random()); xhr.send(null); xhr.onreadystatechange = function () {";
+            strResp += "function getswitches() {var xhr2 = new XMLHttpRequest(); xhr2.open('GET', 'switch.aspx?" + securityKey + "&no=1&_=' + Math.random()); xhr2.send(null); xhr2.onreadystatechange = function () {";
+            strResp += "if (xhr2.readyState == 4) {";
+            strResp += "for (inc = 0; inc < NumberSwitch; inc++) { if(xhr2.responseText.charAt(inc)=='0') mynum = 0; else mynum = 1; buildSwitch('switch'+inc, mynum);}}};}";
+            //signals
+            strResp += "function buildSignal(boxMSG, num) {var obj = document.getElementById(boxMSG); ";
+            strResp += "if (num == 1) obj.src = 'file/green.png'; else obj.src = 'file/red.png';}";
+            strResp += "var NumberSignal=" + mySignal.NumberOfSignals + ";";
+            strResp += "function getsignals() {var xhr = new XMLHttpRequest(); xhr.open('GET', 'signal.aspx?" + securityKey + "&no=1&_=' + Math.random()); xhr.send(null); xhr.onreadystatechange = function () {";
             strResp += "if (xhr.readyState == 4) {";
-            strResp += "for (inc = 0; inc < NumberSwitch; inc++) { if(xhr.responseText.charAt(inc)=='0') mynum = 0; else mynum = 1; buildSwitch('switch'+inc, mynum);}}};}";
+            strResp += "for (inc = 0; inc < NumberSignal; inc++) { if(xhr.responseText.charAt(inc)=='0') mynum = 0; else mynum = 1; buildSignal('signal'+inc, mynum);}}};}";
+            //initialization
+            strResp += "function InitAll() {getsignals();getswitches();}";
             strResp += "</SCRIPT>";
             //strResp = WebServer.OutPutStream(response, strResp);
             // Create one section for each train
@@ -782,6 +796,11 @@ namespace LegoTrain
                 //strResp += "<TR><FORM><TD>" + myParamRail.Switchs[i].Name + "</TD><TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('switch" + i + "'),'" + pageSwitch + "?si=" + i + "&md=0&no=1')\" value=\"|\"></TD>";
                 //strResp += "<TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('switch" + i + "'),'" + pageSwitch + "?si=" + i + "&md=1&no=1')\" value=\"/\"></TD>";
                 //strResp += "<TD><span id='switch" + i + "'></span></FORM></TD></TR>";
+            }
+            for (byte i = 0; i < myParamRail.NumberOfSignals; i++)
+            {
+                strResp += "<span style='position: absolute;margin-left:" + myParamRail.Signals[i].Left + "px; margin-top:" + myParamRail.Signals[i].Top + "px; top:findTop(document.all.MyImage); left:findLeft(document.all.MyImage);' >";
+                strResp += "<img border='0' src=\"\" id='signal" + i + "' onClick=\"siclicked(document.getElementById('signal" + i + "'),'si=" + i + "&no=1');\" /></span>";
             }
             strResp += "<img alt='Map of the city' id='MyImage' src='file/circuit.png' /><a href='all.aspx?" + securityKey + "'>Display all page</a>";
             strResp += "</body></html>";
@@ -824,12 +843,19 @@ namespace LegoTrain
                 strResp += "<TD><span id='train" + i + "'></span></FORM></TD></TR>";
                 //strResp = WebServer.OutPutStream(response, strResp);
             }
-
+            strResp += "<TR><TD>&nbsp</TD></TR>";
             for (byte i = 0; i < myParamRail.NumberOfSwitchs; i++)
             {
                 strResp += "<TR><FORM><TD>" + myParamRail.Switchs[i].Name + "</TD><TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('switch" + i + "'),'" + pageSwitch + "?si=" + i + "&md=0&no=1')\" value=\"|\"></TD>";
                 strResp += "<TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('switch" + i + "'),'" + pageSwitch + "?si=" + i + "&md=1&no=1')\" value=\"/\"></TD>";
                 strResp += "<TD><span id='switch" + i + "'></span></FORM></TD></TR>";
+            }
+            strResp += "<TR><TD>&nbsp</TD></TR>";
+            for (byte i = 0; i < myParamRail.NumberOfSignals; i++)
+            {
+                strResp += "<TR><FORM><TD>Signal " + myParamRail.Signals[i].Name + "</TD><TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('signal" + i + "'),'" + pageSignal + "?si=" + i + "&md=0&no=1')\" value=\"Red\"></TD>";
+                strResp += "<TD><INPUT type=\"button\" onClick=\"btnclicked(document.getElementById('signal" + i + "'),'" + pageSignal + "?si=" + i + "&md=1&no=1')\" value=\"Green\"></TD>";
+                strResp += "<TD><span id='signal" + i + "'></span></FORM></TD></TR>";
             }
             strResp += "</TABLE><br><a href='all.aspx?" + securityKey + "'>Display all page</a><br><a href='circ.aspx?" + securityKey + "'>Circuit</a>";
             strResp += "</body></html>";
